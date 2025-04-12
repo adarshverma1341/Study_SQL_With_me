b6f38aab090f89c2fe2817249a277dcc74a213c7
Adarsh Verma <adarshverma1341@gmail.com>
2025-04-12 20:58:25 +0530

    Add files via upload

---
+++ Select_Statements_SQL.sql
@@ -0,0 +1,26 @@
+-- SQL SELECT STATEMENT
+/* The SELECT Statement is used to select data from database */
+--SELECT column_name FROM table_name;
+
+
+/* To Select all the fields available in the Table*/
+-- SELECT * FROM Table_name;
+
+
+/* To Select DISTINCT/UNIQUE column in the table*/
+--SELECT Column_Name FROM Table_Name
+
+
+----Now let's see the Examples--
+
+Use weekday_SQL_2025
+Select * From Emp_6
+
+Select E_Name From Emp_6
+
+Select Distinct E_Salary From Emp_6
+
+/* Where Clause */
+
+Select * From Emp_6
+Where E_Name= 'Nikhil'
