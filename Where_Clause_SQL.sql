282855be8d98f9163c5130c5378f9c3ade9bb872
Adarsh Verma <adarshverma1341@gmail.com>
2025-04-16 13:09:30 +0530

    Add files via upload

---
+++ Where_Clause_SQL.sql
@@ -0,0 +1,20 @@
+-- How To use Where Clause
+
+use AV_School
+
+Select * From	E_Salary_Details
+
+Select E_Name From E_Salary_Details
+Where E_Salary=15000
+
+Select * From E_Salary_Details
+Where E_Salary=15000
+
+Select * From E_Salary_Details
+Where E_Name='Sunil'
+
+Select * From E_Salary_Details
+Where E_Salary>=35000
+
+Select * From E_Salary_Details
+Where E_Salary<=35000
